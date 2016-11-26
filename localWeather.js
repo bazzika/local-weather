eeeac9ef619c23b8ad0a8f371b86e64d083e5272
bazzika <darya.belob@gmail.com>
2016-11-26 16:28:23 +0200

    - small refactoring and css rename

--- localWeather.js
+++ localWeather.js
@@ -10,65 +10,65 @@ ymaps.ready(function () {
     fog: 'images/fog.png',
     hail: 'images/hail.png',
     'partly-cloudy-day': 'images/partly-cloudy-day.png',
-    'partly-cloudy-night': 'images/partly-cloudy-night.png',
+    'partly-cloudy-night': 'images/bluesky.jpg',
     snow: 'images/snow.jpg',
     sleet: 'images/icons/sleet.png',
     thunderstorm: 'images/icons/thunderstorm.jpg'
   };
-  var DEGREE_FAREINGEIT='°F';
-  var DEGREE_CELCIUM='°C';
-  var latitude=ymaps.geolocation.latitude;
-  var longitude=ymaps.geolocation.longitude;
-  var clientKey='f9e2f4de6611a0ad0d4af7f59750bbe9';
+  var DEGREE_FAREINGEIT = '°F';
+  var DEGREE_CELCIUM = '°C';
+  var latitude = ymaps.geolocation.latitude;
+  var longitude = ymaps.geolocation.longitude;
+  var clientKey = 'f9e2f4de6611a0ad0d4af7f59750bbe9';
   $("#user-city").text(ymaps.geolocation.city);
   $("#user-country").text(ymaps.geolocation.country);
-  var api = 'https://api.darksky.net/forecast/'+clientKey+'/'+latitude + ',' + longitude;
+  var api = 'https://api.darksky.net/forecast/' + clientKey + '/' + latitude + ',' + longitude;
   console.log(api);
   $.ajax({
     url: api,
     type: 'GET',
     dataType: 'jsonp',
     success: function (data) {
-      var currentWeatherStatus=data.currently;
+      var currentWeatherStatus = data.currently;
       showWeather({
-        temperature:currentWeatherStatus.temperature,
-        summary:currentWeatherStatus.summary,
-        windSpeed:currentWeatherStatus.windSpeed,
-        icon:currentWeatherStatus.icon,
-        humidity:currentWeatherStatus.humidity
+        temperature: currentWeatherStatus.temperature,
+        summary: currentWeatherStatus.summary,
+        windSpeed: currentWeatherStatus.windSpeed,
+        icon: currentWeatherStatus.icon,
+        humidity: currentWeatherStatus.humidity
       });
-      changeBackgroundImage(currentWeatherStatus.icon);
-      toggleButton(currentWeatherStatus.temperature);
+      toggleUnit(currentWeatherStatus.temperature);
     },
     error: function (err) {
       throw err;
     }
   });
   function showWeather(currentWeather) {
-    $('.degree').text(Math.round(currentWeather.temperature));
-    $('.unit').text(DEGREE_FAREINGEIT);
-    $('.description').text(currentWeather.summary);
-    $('.humidity').text(currentWeather.humidity*100 + '%');
-    $('.wind').text(Math.round(currentWeather.windSpeed*0.44704) + 'm/s');
-    $('.icon').prop('src', 'images/icons/' + currentWeather.icon + '.png');
+    //TODO: use map as for icon as for image
+    var imageURL = mapIconsToBackgroundImages[currentWeather.icon];
+    $('.js-degree').text(Math.round(currentWeather.temperature));
+    $('.js-unit').text(DEGREE_FAREINGEIT);
+    $('.js-description').text(currentWeather.summary);
+    $('.js-humidity').text(currentWeather.humidity * 100 + '%');
+    $('.js-wind').text(Math.round(currentWeather.windSpeed * 0.44704) + 'm/s');
+    $('.js-icon').prop('src', 'images/icons/' + currentWeather.icon + '.png');
+    $('.js-background').css("background-image", 'url(' + imageURL + ')');
   }
 
-  function changeBackgroundImage(icon) {
-    var imageURL = mapIconsToBackgroundImages[icon];
-    $('.wrapper').css("background-image", 'url(' + imageURL + ')');
-  }
-  function toggleButton(temperature) {
-    var $unit = $('.unit');
-    var $degree = $('.degree');
+  function toggleUnit(temperature) {
+    var $unit = $('.js-unit');
+    var $degree = $('.js-degree');
     $unit.on('click', function () {
-//                $unit.text('�F');
-      if ($unit.text()===DEGREE_FAREINGEIT) {
-        $degree.text(Math.round((temperature - 32)*5/9));
-        $unit.text(DEGREE_CELCIUM);
+      var measure = DEGREE_FAREINGEIT;
+      var currentTemperature = temperature;
+      if ($unit.text() === DEGREE_FAREINGEIT) {
+        currentTemperature = Math.round((currentTemperature - 32) * 5 / 9);
+        measure = DEGREE_CELCIUM;
       } else {
-        $degree.text(Math.round((temperature)));
-        $unit.text(DEGREE_FAREINGEIT);
+        currentTemperature = Math.round((currentTemperature));
       }
+      $degree.text(currentTemperature);
+      $unit.text(measure);
     });
   }
 
